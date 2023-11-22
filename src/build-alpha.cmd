b67fa0dc11e8d64e15d9ccb081a7ffa66b9ba231
timglewis <tim.lewis@gmail.com>
2023-11-22 13:29:45 +1100

    Push to origin

--- src/build-alpha.cmd
+++ src/build-alpha.cmd
@@ -7,4 +7,10 @@ dotnet build -c:Release --version-suffix %VERSIONSUFFIX% SharpGLTF.Core\SharpGLT
 dotnet build -c:Release --version-suffix %VERSIONSUFFIX% SharpGLTF.Runtime\SharpGLTF.Runtime.csproj
 dotnet build -c:Release --version-suffix %VERSIONSUFFIX% SharpGLTF.Toolkit\SharpGLTF.Toolkit.csproj
 
+set DSTPATH=C:\Users\timle\source\repos\KeyframeAI\Keyframe.SpeckleTiler\SpeckleTiler\Packages
+
+copy SharpGLTF.Core\bin\Release\*.*nupkg    %DSTPATH%
+copy SharpGLTF.Runtime\bin\Release\*.*nupkg %DSTPATH%
+copy SharpGLTF.Toolkit\bin\Release\*.*nupkg %DSTPATH%
+
 pause
